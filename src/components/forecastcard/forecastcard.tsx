d3dc2def3e694db06d861fe1d81a513a7b7b5327
Anis Almukhametov <anisclient@gmail.com>
2021-05-20 21:25:02 +0300

    feat(selectcity): add selectcity component

--- src/components/forecastcard/forecastcard.tsx
+++ src/components/forecastcard/forecastcard.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Selectcity from '../select'
 import './forecastcard.scss'
 
 const Forecastcard: React.FC = () => {
   return (
     <section className="main__forecastcard forecastcard">
       <h3 className="forecastcard__title">7 Days Forecast</h3>
+      <Selectcity className="forecastcard__selectcity" />
     </section>
   )
 }
---
+++ src/components/select/selectcity.tsx
@@ -0,0 +1,86 @@
+import React, { useRef, useState } from 'react'
+import './selectcity.scss'
+import cn from 'classnames'
+
+interface City {
+  id: number
+  name: string
+}
+
+interface SelectProps {
+  className?: string
+}
+
+const cities: City[] = [
+  { id: 1, name: 'Sankt Petersburg' },
+  { id: 2, name: 'Saratov' },
+  { id: 3, name: 'Samara' },
+  { id: 4, name: 'Saransk' },
+]
+
+const Selectcity: React.FC<SelectProps> = ({ className }) => {
+  const selectcityRef = useRef() as React.MutableRefObject<HTMLInputElement>
+
+  const [value, setValue] = useState('')
+  const [active, setActive] = useState(false)
+
+  function setNameToInput(name: string) {
+    setValue(name)
+    setActive(false)
+    selectcityRef.current.focus()
+  }
+
+  function setNameToInputWithKeyboard(e: React.KeyboardEvent, name: string) {
+    const keyCode = e.key
+
+    if (keyCode === 'Enter') {
+      setValue(name)
+      setActive(false)
+      selectcityRef.current.focus()
+    }
+  }
+
+  function handleWithKeyboard(e: React.KeyboardEvent) {
+    const keyCode = e.key
+    console.log('keyCode-->', keyCode)
+
+    if (keyCode === 'ArrowDown') {
+      setActive(true)
+    }
+
+    if (keyCode === 'ArrowUp') {
+      setActive(false)
+    }
+  }
+
+  return (
+    <form className={cn(className, 'selectcity')}>
+      <input
+        value={value}
+        type="text"
+        readOnly
+        ref={selectcityRef}
+        placeholder="Select city"
+        className={cn('selectcity__input', `${active && 'active'}`)}
+        onClick={() => setActive(!active)}
+        onKeyDown={(e) => handleWithKeyboard(e)}
+      />
+      {active && (
+        <ul className="selectcity__items" tabIndex={0}>
+          {cities.map((city) => (
+            <li
+              className="selectcity__item"
+              key={city.id}
+              tabIndex={0}
+              onClick={() => setNameToInput(city.name)}
+              onKeyDown={(e) => setNameToInputWithKeyboard(e, city.name)}>
+              {city.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </form>
+  )
+}
+
+export default Selectcity
