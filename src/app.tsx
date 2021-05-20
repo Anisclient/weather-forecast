765b6e74e8e522a8e392d5316cc229c7be05b7f0
Anis Almukhametov <anisclient@gmail.com>
2021-05-20 15:45:40 +0300

    feat(Main): add main file

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './app.scss'
 import Footer from './components/footer'
 import Header from './components/header'
+import Main from './components/main'
 
 const App: React.FC = () => {
   return (
     <div className="container">
       <Header />
+      <Main />
       <Footer />
     </div>
   )
