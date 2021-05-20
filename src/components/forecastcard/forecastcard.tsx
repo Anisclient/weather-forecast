f7d957eab06ac02af6e22edcf7e9955e08220e89
Anis Almukhametov <anisclient@gmail.com>
2021-05-20 16:12:44 +0300

    feat(forecastcard): add title to forecastcard

--- src/components/forecastcard/forecastcard.tsx
+++ src/components/forecastcard/forecastcard.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './forecastcard.scss'
 
 const Forecastcard: React.FC = () => {
-  return <section className="main__forecastcard forecastcard">card</section>
+  return (
+    <section className="main__forecastcard forecastcard">
+      <h3 className="forecastcard__title">7 Days Forecast</h3>
+    </section>
+  )
 }
 
 export default Forecastcard
