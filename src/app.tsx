6cb72d9abdd3088e75b469cb96245deb822a3dc1
Anis Almukhametov <anisclient@gmail.com>
2021-05-20 15:38:51 +0300

    feat(Footer): add footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './app.scss'
+import Footer from './components/footer'
 import Header from './components/header'
 
 const App: React.FC = () => {
   return (
     <div className="container">
       <Header />
+      <Footer />
     </div>
   )
 }
