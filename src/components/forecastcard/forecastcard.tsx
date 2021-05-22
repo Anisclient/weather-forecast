91321dccf11faba019d5ff006cd025a956819599
Anis Almukhametov <anisclient@gmail.com>
2021-05-22 12:36:13 +0300

    refactor(Main, Forecastcard): change Main and ForecastCard depending on selectcity and selectdate

--- src/components/forecastcard/forecastcard.tsx
+++ src/components/forecastcard/forecastcard.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import Placeholder from '../placeholder'
 import Selectcity from '../selectcity'
+import Selectdate from '../selectdate'
 import './forecastcard.scss'
 
 interface ForecastcardProps {
-  title: '7 Days Forecast' | 'Forecast for a Date in the Past'
+  type: '7days' | 'dateInThePast'
 }
 
-const Forecastcard: React.FC<ForecastcardProps> = ({ title }) => {
+const Forecastcard: React.FC<ForecastcardProps> = ({ type }) => {
   return (
     <section className="main__forecastcard forecastcard">
-      <h3 className="forecastcard__title">{title}</h3>
+      <h3 className="forecastcard__title">
+        {type === '7days' ? '7 Days Forecast' : 'Forecast for a Date in the Past'}
+      </h3>
       <div className="forecastcard__selectgroup">
-        <Selectcity className="forecastcard__selectcity" />
-        <Selectcity className="forecastcard__selectcity" />
+        {type === '7days' ? (
+          <Selectcity />
+        ) : (
+          <>
+            <Selectcity className="forecastcard__selectcity" />
+            <Selectdate className="forecastcard__selectcity" />
+          </>
+        )}
       </div>
       <Placeholder className="forecastcard__placeholder" />
     </section>
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,8 +5,8 @@ import './main.scss'
 const Main: React.FC = () => {
   return (
     <main className="main">
-      <Forecastcard title="7 Days Forecast" />
-      <Forecastcard title="Forecast for a Date in the Past" />
+      <Forecastcard type="7days" />
+      <Forecastcard type="dateInThePast" />
     </main>
   )
 }
