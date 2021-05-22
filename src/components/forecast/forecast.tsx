b18f1e0d53af5e14c8a097015a40c0336b3cd3b2
Anis Almukhametov <anisclient@gmail.com>
2021-05-22 18:16:47 +0300

    refactor(Forecast): make nowrap words in date title

--- src/components/forecast/forecast.tsx
+++ src/components/forecast/forecast.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './forecast.scss'
+import cn from 'classnames'
 import thunderstorm from '../../assets/thunderstorm.png'
 
-const Forecast = () => {
+interface ForecastProps {
+  className?: string
+}
+
+const Forecast: React.FC<ForecastProps> = ({ className }) => {
   return (
-    <div className="forecast">
-      <p className="forecast__date">29 sep 2021</p>
+    <div className={cn(className, 'forecast')}>
+      <div className="forecast__date">
+        <p>29 sep 2021</p>
+      </div>
       <img
         src={thunderstorm}
         // todo
