4bf8a20e4197705c9e2dc84f5d12cfa2d0706bc8
Anis Almukhametov <anisclient@gmail.com>
2021-05-23 16:49:42 +0300

    feat: fetch 7 days  Kazan weather forecast

--- src/components/selectcity/selectcity.tsx
+++ src/components/selectcity/selectcity.tsx
@@ -1,10 +1,12 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './selectcity.scss'
 import cn from 'classnames'
 
 interface City {
   id: number
   name: string
+  lat: number
+  lon: number
 }
 
 interface SelectCityProps {
@@ -12,10 +14,11 @@ interface SelectCityProps {
 }
 
 const cities: City[] = [
-  { id: 1, name: 'Sankt Petersburg' },
-  { id: 2, name: 'Saratov' },
-  { id: 3, name: 'Samara' },
-  { id: 4, name: 'Saransk' },
+  { id: 1, name: 'Самара', lat: 53.195873, lon: 50.100193 },
+  { id: 2, name: 'Тольятти', lat: 53.507836, lon: 49.420393 },
+  { id: 3, name: 'Саратов', lat: 51.533557, lon: 46.034257 },
+  { id: 4, name: 'Казань', lat: 55.796127, lon: 49.106405 },
+  { id: 5, name: 'Краснодар', lat: 45.03547, lon: 38.975313 },
 ]
 
 const Selectcity: React.FC<SelectCityProps> = ({ className }) => {
@@ -23,6 +26,18 @@ const Selectcity: React.FC<SelectCityProps> = ({ className }) => {
 
   const [value, setValue] = useState('')
   const [active, setActive] = useState(false)
+
+  function fetchForecast(city: City) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lon}&units=metric&lang=ru&exclude=current,minutely,hourly&appid=a8bda3d6c35729ebd9a60a4aedcb4178`,
+    )
+      .then((data) => data.json())
+      .then((res) => console.log('res', res))
+  }
+
+  useEffect(() => {
+    fetchForecast(cities[3])
+  }, [])
 
   function setCityNameToInput(name: string) {
     setValue(name)
