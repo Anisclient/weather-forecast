fbd3992e0085bcb928d9bafa0817363bd3e7a88e
Anis Almukhametov <anisclient@gmail.com>
2021-05-23 17:24:29 +0300

    feat(app): add store to app

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,18 @@ import './app.scss'
 import Footer from './components/footer'
 import Header from './components/header'
 import Main from './components/main'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </Provider>
   )
 }
 
